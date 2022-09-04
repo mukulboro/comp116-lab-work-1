80e0557cd0cfd62b0971cc5b80a6ac3a268175a2
Mukul Aryal <aryalmukul@gmail.com>
2022-09-04 11:10:32 +0545

    Completed Q No. 2

--- question2.cpp
+++ question2.cpp
@@ -1,6 +1,17 @@
 // To return multiple values from a function
 #include<iostream>
 
+int* returnMultiple(int n1, int n2);
+
 int main(){
+    int* output = returnMultiple(6, 9);
+    std::cout<<output[0]<<" & "<<output[1]<<" were returned from function."<<std::endl;
     return 0;
+}
+
+int* returnMultiple(int n1, int n2){
+    int *array;
+    array[0] = n1+n2;
+    array[1] = n1*n2;
+    return array;
 }
