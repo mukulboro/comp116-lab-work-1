3b51b82365cdf195edc002427dfec09c065ebc86
Mukul Aryal <aryalmukul@gmail.com>
2022-09-04 11:13:10 +0545

    Completed Q No. 4

--- question4.cpp
+++ question4.cpp
@@ -1,6 +1,16 @@
 // To swap two numbers (using references)
 #include<iostream>
 
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 int main(){
+    int n1= 6, n2=9;
+    std::cout<<"BEFORE SWAP: "<<n1<<" & "<<n2<<std::endl;
+    swap(n1, n2);
+    std::cout<<"AFTER SWAP: "<<n1<<" & "<<n2<<std::endl;
     return 0;
 }
