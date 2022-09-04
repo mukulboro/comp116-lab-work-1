5a68f91a6d86131d6da3b106bcde92e2dda6bef4
Mukul Aryal <aryalmukul@gmail.com>
2022-09-04 11:05:08 +0545

    Completed Q No. 1

--- question1.cpp
+++ question1.cpp
@@ -2,5 +2,10 @@
 #include<iostream>
 
 int main(){
+    char userInput; int asciiValue;
+    std::cout<<"Enter a character: "<<std::endl;
+    std::cin>>userInput;
+    asciiValue = int(userInput);
+    std::cout<<"ASCII value of "<<userInput<<" is "<<asciiValue<<std::endl;
     return 0;
 }
